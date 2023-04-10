efab9ccd6912e5ce2567a69f875a1d8a9b3d3c93
rishavchanda <rishavchanda0@gmail.com>
2023-04-10 10:08:50 +0530

    mage changes in the server

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-#use fast api taking string as parameter from a get function and converting it to image and then to numpy array and then to a dataframe and then to a prediction and then to a json file and then to a string and then to a response
-from flask import Flask,json,request
+# use fast api taking string as parameter from a get function and converting it to image and then to numpy array and then to a dataframe and then to a prediction and then to a json file and then to a string and then to a response
+from flask import Flask, json, request
 from tensorflow.keras.models import model_from_json
 from flask_cors import CORS, cross_origin
 import numpy as np
@@ -7,20 +7,24 @@ import pandas as pd
 import cv2
 import pickle
 import base64
+from io import BytesIO
+from PIL import Image
 from typing import List
 from pydantic import BaseModel
+import tensorflow as tf
 
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
-#model = pickle.load(open("brain_tumor_model.pkl", "rb"))
+# model = pickle.load(open("brain_tumor_model.pkl", "rb"))
 json_file = open('model.json', 'r')
 loaded_model_json = json_file.read()
 json_file.close()
 loaded_model = model_from_json(loaded_model_json)
 # load weights into new model
 loaded_model.load_weights("model.h5")
+
 
 def get_cv2_image_from_base64_string(b64str):
     encoded_data = b64str.split(',')[1]
@@ -29,25 +33,39 @@ def get_cv2_image_from_base64_string(b64str):
     return img
 
 
-@app.route("/",methods=['POST'])
+def get_image_from_base64_string(b64str):
+    encoded_data = b64str.split(',')[1]
+    image_data = BytesIO(base64.b64decode(encoded_data))
+    img = Image.open(image_data)
+    return img
+
+
+@app.route("/", methods=['POST'])
 def read_root():
     data = json.loads(request.data)
-    predict_img=[]
+    predict_img = []
     for item in data['image']:
-        # Decode the base64-encoded image
-        image = get_cv2_image_from_base64_string(item)
-        image = cv2.resize(image,(224,224))
-        predict_img.append(image)
+        encoded_data = item.split(',')[1]
+        image_data = BytesIO(base64.b64decode(encoded_data))
+        pil_image = Image.open(image_data)
+        # Resizing the image to 224x224
+        resized_image = pil_image.resize((224, 224))
+        # Appending the resized image to the list
+        predict_img.append(resized_image)
 
-    # # Convert the image to a numpy array
-    prediction = loaded_model.predict(np.array(predict_img))
-    result = np.argmax(prediction, axis =1)
+    np_images = np.array([np.array(img) for img in predict_img])
+    # Converting the NumPy array to a TensorFlow tensor
+    tf_images = tf.convert_to_tensor(np_images, dtype=tf.float32)
+    # # Converting the image to a numpy array
+    prediction = loaded_model.predict(tf_images)
+    result = np.argmax(prediction, axis=1)
 
-    #make the probablity frtom prediction
+    # make the probablity frtom prediction
     # print(prediction[:,1])
     # print(result)
 
-    return {"result": prediction[:,1].tolist()}
+    return {"result": prediction[:, 1].tolist()}
+
 
 if __name__ == '__main__':
-  app.run(port=5000)
+    app.run(port=5000)
