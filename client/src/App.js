faf29a1b6c1dc8ea56a9876b7ed47f37b422b133
rishavchanda <rishavchanda0@gmail.com>
2023-05-03 17:16:58 +0530

    changed the server url

--- client/src/App.js
+++ client/src/App.js
@@ -130,7 +130,7 @@ function App() {
       imageData.push(images[i].base64_file)
     }
     const data = { image: imageData }
-    const res = await axios.post('https://brain-tumor-detection-production.up.railway.app/', data).catch((err) => {
+    const res = await axios.post('https://brain-tumor-detection-b5qi.onrender.com/', data).catch((err) => {
       console.log(err);
     });
     setPredictedImage(images)
